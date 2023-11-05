d44f38564a9fba1f53d68b008db758967937519b
Sesame09 <hm0912l@naver.com>
2023-11-05 19:24:31 +0900

    2023.11.05 modifided -5

--- src/CharacterDesign.js
+++ src/CharacterDesign.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import X from './assets/icons/X.svg';
-import Next from './assets/icons/next.svg';
-import Previous from './assets/icons/previous.svg';
 import { icons, characterDesigns } from './images';
+import Modal from './ImageModal';
 import './CharacterDesign.css';
 
 
@@ -20,17 +18,16 @@ function CharacterDesign() {
     setModalOpen(false);
   };
 
-  const prevImage = () => {
-    if (currentImageIndex > 0) {
-      setCurrentImageIndex(currentImageIndex - 1);
-    }
-  };
-
-  const nextImage = () => {
-    if (currentImageIndex < characterDesigns.length - 1) {
-      setCurrentImageIndex(currentImageIndex + 1);
-    }
-  };
+  const images = characterDesigns.map((image, index) => (
+    <div key={index} className='grid-item'>
+      <img
+        src={image}
+        alt={`${index + 1}`}
+        onClick={() => openModal(index)}
+        className='grid-image'
+      />
+    </div>
+  ));
 
 
   return (
@@ -42,42 +39,20 @@ function CharacterDesign() {
         <img className="sparkle-title" src={icons.sparkleTitle} alt="sparkle"/>
       </div>
 
-      <div className="illusts">
-        <div className="grid-container-CD">
-          {characterDesigns.map((image, index) => (
-            <div key={index} className="grid-item">
-              <img
-                src={image}
-                alt={`${index + 1}`}
-                onClick={() => openModal(index)}
-                className="grid-image"
-              />
-            </div>
-          ))}
+      <div className='characterDesigns'>
+        <div className='grid-container'>
+          {images}
         </div>
       </div>
 
-          {modalOpen && (
-            <div className="illust-modal">
-              <button className="close-button" onClick={closeModal}>
-                <img src={X} className='close-img' alt='close'/>
-              </button>
-              <div className="modal-buttons">
-                <button className="prev-button" onClick={prevImage}>
-                  <img src={Previous} className="prev-img" alt="previous" />
-                </button>
-                <button className="next-button" onClick={nextImage}>
-                  <img src={Next} className="next-img" alt="next" />
-                </button>
-              </div>
-              <img
-                className="modal-image"
-                src={characterDesigns[currentImageIndex]}
-                alt={`${currentImageIndex + 1}`}
-              />
-              
-            </div>
-          )}
+      {modalOpen && (
+        <Modal 
+          images={characterDesigns} 
+          index={currentImageIndex} 
+          close={closeModal} 
+        />
+      )}
+
     
     </div>
   );
